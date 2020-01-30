28331e6d95d062cabe31a66a01459e76e519e5a2
wangdong20 <2865678894@qq.com>
2020-01-30 10:40:46 -0800

    add custom toast frame with shadow effect

--- app/src/main/java/com/com490/expiredteam/expired/MainActivity.kt
+++ app/src/main/java/com/com490/expiredteam/expired/MainActivity.kt
@@ -2,11 +2,25 @@ package com.com490.expiredteam.expired
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        manual_input_btn.setOnClickListener {
+            // Test code for custom toast.
+            var toastView = LayoutInflater.from(this).inflate(R.layout.custom_toast, findViewById(R.id.custom_toast_layout))
+            var toast = Toast(this)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.duration = Toast.LENGTH_LONG
+            toast.view = toastView
+            toast.show()
+        }
     }
 }
