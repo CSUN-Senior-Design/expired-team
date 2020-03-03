8760a184247e92512fe05d2f06fefa93f033228f
wangdong20 <2865678894@qq.com>
2020-03-03 10:05:55 -0800

    merge Kevin's work and delete redundant files

--- app/src/main/java/com/com490/expiredteam/expired/ProductPage.java
+++ app/src/main/java/com/com490/expiredteam/expired/ProductPage.java
@@ -1,6 +1,6 @@
 package com.com490.expiredteam.expired;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProductPage extends AppCompatActivity {
